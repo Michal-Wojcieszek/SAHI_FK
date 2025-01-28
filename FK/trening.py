c0143a481f4ec5235cc7a4200618bc629f465f25
Michal-Wojcieszek <164656774+Michal-Wojcieszek@users.noreply.github.com>
2025-01-28 18:51:43 +0100

    Update trening.py

--- FK/trening.py
+++ FK/trening.py
@@ -1,8 +1,6 @@
 from ultralytics import YOLO
 
-# Train the model
 if __name__ == '__main__':
-    # Load a model
-    modeln = YOLO(r"models/yolo11n.pt")  # load a pretrained model (recommended for training)
+    modeln = YOLO(r"models/yolo11n.pt") 
     resultsn = modeln.train(data=r"SODA-D/soda_d_slice.yaml", epochs=100, imgsz=640, device=[0] , batch=64, resume=False, save_period = 1, plots=True)
     resultsn = modeln.val()
